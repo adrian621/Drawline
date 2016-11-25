e55b97a416b5a2f795675a58188e737c065938a1
Adrian <adty3240>
2016-11-25 13:50:35 +0100

    Lagt in socket.io i server och testat connecta en client

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server, {});
 
 //specifie folder to use for static pagaes such as css scripts
-//app.use(express.static(''));
+app.use(express.static('public'));
 
 //Handele all get requests from clients.
 //send the html file
@@ -18,3 +19,8 @@ app.get('/', function(req, res){
 //Set up server to listen to port 2000
 server.listen(process.env.PORT || 2000);
 console.log('server is running');
+
+io.sockets.on('connection', function(socket){
+	console.log('client connected');
+			
+});
